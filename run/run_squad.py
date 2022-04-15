8af407f4d5c1746959cf88576889b2d6269a6be7
josubg <josubg@gmail.com>
2022-04-15 12:06:08 +0200

    clean all flags

--- run/run_squad.py
+++ run/run_squad.py
@@ -34,15 +34,6 @@ flags = tf.flags
 
 FLAGS = flags.FLAGS
 
-####Delete all flags before declare#####
-def del_all_flags(FLAGS):
-    flags_dict = FLAGS._flags()
-    keys_list = [keys for keys in flags_dict]
-    for keys in keys_list:
-        FLAGS.__delattr__(keys)
-
-del_all_flags(tf.flags.FLAGS)
-
 
 ## Required parameters
 flags.DEFINE_string(
